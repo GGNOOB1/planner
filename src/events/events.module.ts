f899d40df635081f4c0dae0bc6ab5ba077a4ac16
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-01 14:05:46 -0300

    Adding entities of event and location

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { EventsController } from './events.controller';
+import { Event } from './events.entity';
 import { EventsService } from './events.service';
+import { Location } from './location.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Event, Location])],
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
 })
 export class EventsModule {}
