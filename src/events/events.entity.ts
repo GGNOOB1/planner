603caaca38d6a9943def62b355f41bfc4ca24d74
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-01 14:04:42 -0300

    Adding more fields

--- src/events/events.entity.ts
+++ src/events/events.entity.ts
@@ -3,20 +3,30 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import { Location } from './location.entity';
 
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
-  _id: number;
+  id: number;
   @Column()
   description: string;
   @Column()
   userId: string;
   @Column()
   dateTime: Date;
+  @OneToOne(() => Location, (location) => location.event)
+  @JoinColumn()
+  location: Location;
   @CreateDateColumn()
   createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
   @Column()
   weekDay: string;
 }
