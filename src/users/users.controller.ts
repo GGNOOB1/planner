7dfa128256d6aaf745637806f00cdf17947dea31
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-02 23:21:33 -0300

    Adding controller and service from user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
+import { Post, Put, Body, Param } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport/dist';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('/api/v1/users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signUp')
+  signUp(@Body() createdUserDto: CreateUserDto) {
+    return this.usersService.create(createdUserDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('/:id')
+  updateUser(@Param('id') id: number, @Body() updatedUser: UpdateUserDto) {
+    return this.usersService.update(id, updatedUser);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createdUserDto: CreateUserDto) {
+    try {
+      if (createdUserDto.password !== createdUserDto.confirmPassword) {
+        throw new BadRequestException('Passwords are not equal');
+      }
+
+      const user = await this.userRepository.create(createdUserDto);
+      return await this.userRepository.save(user);
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  async findOne(email: string) {
+    const user = await this.userRepository.findOneBy({ email });
+
+    return user;
+  }
+
+  async findById(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+
+    return user;
+  }
+
+  async update(id: number, updatedUser: UpdateUserDto) {
+    const user = await this.findById(id);
+    this.userRepository.merge(user, updatedUser);
+    return await this.userRepository.save(user);
+  }
+}
