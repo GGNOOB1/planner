c5ad26aff3d620910fdc41aca9d24b5d56c24d81
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-02 23:16:28 -0300

    Adding authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { Event } from './events/events.entity';
 import { Location } from './events/location.entity';
 import { User } from './users/users.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { User } from './users/users.entity';
     }),
     EventsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
