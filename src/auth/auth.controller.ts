d915f36dcb1ec98def4e8ad5e99252fab984d1d3
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 16:09:07 -0300

    Adding update and deletion routes of users and use guards for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  Put,
+  Param,
+  Body,
+  UseGuards,
+  HttpStatus,
+} from '@nestjs/common';
+import { Delete, HttpCode } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
+import { UpdateUserDto } from 'src/auth/dtos/update-user.dto';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Controller('/api/v1/users')
@@ -10,5 +22,32 @@ export class AuthController {
   @Post('/signIn')
   signIn(@Req() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/refreshToken')
+  refreshToken(@Req() req) {
+    return this.authService.generateNewToken(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('/:id')
+  updateUser(
+    @Req() req,
+    @Param('id') id: number,
+    @Body() updatedUser: UpdateUserDto,
+  ) {
+    const token = req.rawHeaders[1];
+
+    return this.authService.update(token, id, updatedUser);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteUser(@Req() req, @Param('id') id: number) {
+    const token = req.rawHeaders[1];
+
+    return this.authService.delete(token, id);
   }
 }
