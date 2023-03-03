aaf95608f2fcc1ef9a345923f852eff2a485c8b3
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 16:16:50 -0300

    Adding userService

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { User } from 'src/users/users.entity';
+import { UsersService } from 'src/users/users.service';
 import { EventsController } from './events.controller';
 import { Event } from './events.entity';
 import { EventsService } from './events.service';
 import { Location } from './location.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, Location])],
+  imports: [TypeOrmModule.forFeature([Event, Location, User])],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, UsersService],
 })
 export class EventsModule {}
