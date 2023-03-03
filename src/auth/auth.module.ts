e4a74ea9cfff6b9777906d6fb04c5647dbf71f82
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 16:10:46 -0300

    Adding User Service as Provider, adding the user entity to Auth and Modifying JWT Expiration Time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,18 +7,22 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     ConfigModule.forRoot(),
     PassportModule,
     UsersModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, UsersService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
