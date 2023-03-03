10a446043669bc8ac65633628ab9d9776a5a3f1c
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 16:17:27 -0300

    Adding Try catch to errors handling

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Event } from './events.entity';
@@ -8,6 +12,10 @@ import { _ } from 'lodash';
 import { Location } from './location.entity';
 import { validadeDays } from './validators/validadeDays';
 import { QueryEvent } from './interfaces/QueryEvent';
+
+import { UsersService } from 'src/users/users.service';
+import { UpdateLocationDto } from './dtos/update-location.dto';
+import { formatError } from '../helpers/formatErrors';
 
 @Injectable()
 export class EventsService {
@@ -25,35 +33,55 @@ export class EventsService {
     @InjectRepository(Event) private eventRepository: Repository<Event>,
     @InjectRepository(Location)
     private locationRepository: Repository<Location>,
+    private userService: UsersService,
   ) {}
 
   async create(
     description: string,
-    userId: string,
+    userId: number,
     dateTime: Date,
     locationName: string,
     latitude: number,
     longitude: number,
   ) {
-    const day = new Date(dateTime).getDay();
-    const weekDay = this.daysWeek[day];
+    try {
+      const user = await this.userService.findById(userId);
 
-    const location = this.locationRepository.create({
-      locationName,
-      latitude,
-      longitude,
-    });
-    await this.locationRepository.save(location);
+      if (!user) {
+        throw new BadRequestException('The user id does not exist');
+      }
 
-    const event = this.eventRepository.create({
-      description,
-      userId,
-      dateTime,
-      weekDay,
-      location,
-    });
+      const day = new Date(dateTime).getDay();
+      const weekDay = this.daysWeek[day];
 
-    return await this.eventRepository.save(event);
+      const location = this.locationRepository.create({
+        locationName,
+        latitude,
+        longitude,
+      });
+      await this.locationRepository.save(location);
+
+      const event = this.eventRepository.create({
+        description,
+        userId,
+        dateTime,
+        weekDay,
+        location,
+      });
+
+      await this.eventRepository.save(event);
+
+      return {
+        id: event.id,
+        description: event.description,
+        userId: event.userId,
+        dateTime: event.dateTime,
+        createdAt: event.createdAt,
+        location: event.location,
+      };
+    } catch (e) {
+      return formatError(e);
+    }
   }
 
   async find(
@@ -62,18 +90,38 @@ export class EventsService {
     offset?: number,
     query?: QueryEvent,
   ) {
-    if (!dayOfTheWeek && !limit && !offset && !query) {
-      return this.eventRepository.find({ relations: ['location'] });
-    }
+    try {
+      if (!dayOfTheWeek && !limit && !offset && !query) {
+        const events = await this.eventRepository.find({
+          relations: ['location'],
+        });
 
-    if (dayOfTheWeek) {
-      dayOfTheWeek = _.capitalize(dayOfTheWeek);
-      dayOfTheWeek = validadeDays(dayOfTheWeek);
+        return events;
+      }
+
+      if (dayOfTheWeek) {
+        dayOfTheWeek = _.capitalize(dayOfTheWeek);
+        dayOfTheWeek = validadeDays(dayOfTheWeek);
+
+        const events = await this.eventRepository
+          .createQueryBuilder('event')
+          .leftJoinAndSelect('event.location', 'location')
+          .where('event.weekDay = :dayOfTheWeek', { dayOfTheWeek })
+          .limit(limit)
+          .offset(offset)
+          .getMany();
+
+        return {
+          limit,
+          offset,
+          total: events.length,
+          items: events,
+        };
+      }
 
       const events = await this.eventRepository
         .createQueryBuilder('event')
         .leftJoinAndSelect('event.location', 'location')
-        .where('event.weekDay = :dayOfTheWeek', { dayOfTheWeek })
         .limit(limit)
         .offset(offset)
         .getMany();
@@ -84,66 +132,86 @@ export class EventsService {
         total: events.length,
         items: events,
       };
+    } catch (e) {
+      return formatError(e);
     }
-
-    const events = await this.eventRepository
-      .createQueryBuilder('event')
-      .leftJoinAndSelect('event.location', 'location')
-      .limit(limit)
-      .offset(offset)
-      .getMany();
-
-    return {
-      limit,
-      offset,
-      total: events.length,
-      items: events,
-    };
   }
 
-  findOne(id: number) {
-    return this.eventRepository.findOneBy({ id });
+  async findOne(id: number) {
+    try {
+      return await this.eventRepository.findOneBy({ id });
+    } catch (e) {}
   }
 
   async findById(id: number) {
-    const event = this.eventRepository.find({
-      where: { id },
-      relations: ['location'],
-    });
-    return event;
+    try {
+      const event = this.eventRepository.find({
+        where: { id },
+        relations: ['location'],
+      });
+      return event;
+    } catch (e) {
+      return formatError(e);
+    }
   }
 
-  async update(id: number, data: UpdateEventDto) {
-    const day = new Date(data.dateTime).getDay();
-    const weekDay = this.daysWeek[day];
-    data.weekDay = weekDay;
+  async update(id: number, updatedEvent: UpdateEventDto) {
+    try {
+      const day = new Date(updatedEvent.dateTime).getDay();
+      const weekDay = this.daysWeek[day];
+      updatedEvent.weekDay = weekDay;
 
-    const event = await this.findOne(id);
+      const event = await this.findOne(id);
 
-    if (!event) {
-      throw new NotFoundException('event not found');
+      if (!event) {
+        throw new NotFoundException({
+          message: 'event not found',
+        });
+      }
+      Object.assign(event, updatedEvent);
+      return this.eventRepository.save(event);
+    } catch (e) {
+      return formatError(e);
     }
-    Object.assign(event, data);
-    return this.eventRepository.save(event);
+  }
+
+  async updateLocation(eventId: number, locations: UpdateLocationDto) {
+    try {
+      const event = await this.findById(eventId);
+      event[0].location.locationName = locations.locationName;
+      event[0].location.latitude = locations.latitude;
+      event[0].location.longitude = locations.longitude;
+      await this.locationRepository.save(event[0].location);
+    } catch (e) {
+      return formatError(e);
+    }
   }
 
   async deleteOne(id: number) {
-    const event = await this.findOne(id);
+    try {
+      const event = await this.findOne(id);
 
-    if (!event) {
-      throw new NotFoundException('event not found');
-    }
+      if (!event) {
+        throw new NotFoundException('event not found');
+      }
 
-    return this.eventRepository.remove(event);
+      return this.eventRepository.remove(event);
+    } catch (e) {}
   }
 
   async delete(weekDay: string) {
-    weekDay = _.capitalize(weekDay);
-    const events = await this.eventRepository.find({ where: { weekDay } });
+    try {
+      weekDay = _.capitalize(weekDay);
+      const events = await this.eventRepository.find({ where: { weekDay } });
 
-    if (!events) {
-      throw new NotFoundException('event not found');
+      if (!events) {
+        throw new NotFoundException('event not found');
+      }
+      await this.eventRepository.remove(events);
+
+      return null;
+    } catch (e) {
+      return formatError(e);
     }
-    return this.eventRepository.remove(events);
   }
 }
