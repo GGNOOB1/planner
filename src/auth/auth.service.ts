a6dc45242446f610530407df6717f7e843cc5a8a
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 16:12:14 -0300

    Login, Update and User delete methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { User } from 'src/users/users.entity';
 import { UsersService } from 'src/users/users.service';
 import { compareSync } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { UpdateUserDto } from 'src/auth/dtos/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { formatError } from 'src/helpers/formatErrors';
+import { getPayLoad } from './helpers/getPayload';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
     private userService: UsersService,
     private jwtService: JwtService,
   ) {}
@@ -25,13 +31,64 @@ export class AuthService {
   }
 
   async login(user: User) {
-    const payload = {
-      sub: user.id,
-      email: user.email,
-    };
+    try {
+      const payload = getPayLoad(user);
 
-    return await {
-      token: this.jwtService.sign(payload),
-    };
+      return await {
+        token: this.jwtService.sign(payload),
+      };
+    } catch (e) {
+      return formatError(e);
+    }
+  }
+
+  async generateNewToken(user: User) {
+    try {
+      const payload = getPayLoad(user);
+
+      return await {
+        token: this.jwtService.sign(payload),
+      };
+    } catch (e) {
+      return formatError(e);
+    }
+  }
+
+  async update(token: string, id: number, updatedUser: UpdateUserDto) {
+    try {
+      const onlyToken = token.split(' ');
+      const tokenIsTruty = await this.jwtService.verify(onlyToken[1], {
+        secret: process.env.JWT_SECRET_KEY,
+      });
+
+      if (id == tokenIsTruty.sub) {
+        const user = await this.userService.findById(id);
+        await this.userRepository.merge(user, updatedUser);
+        return await this.userRepository.save(user);
+      } else {
+        throw new UnauthorizedException('You can only update your data!');
+      }
+    } catch (e) {
+      return formatError(e);
+    }
+  }
+
+  async delete(token, id: number) {
+    try {
+      const onlyToken = token.split(' ');
+      const tokenIsTruty = await this.jwtService.verify(onlyToken[1], {
+        secret: process.env.JWT_SECRET_KEY,
+      });
+
+      if (id == tokenIsTruty.sub) {
+        const user = await this.userService.findById(id);
+        await this.userRepository.remove(user);
+        return null;
+      } else {
+        throw new UnauthorizedException('You can only update your data!');
+      }
+    } catch (e) {
+      return formatError(e);
+    }
   }
 }
