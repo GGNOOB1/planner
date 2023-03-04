14cbcb0839ac2c6edbcff70f2daa259a0dc155ca
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-03 21:40:45 -0300

    Configuring swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Planner API')
+    .setDescription(
+      'Third project in the compass grant program to develop a planning API to improve node js, ' +
+        'nestjs, sqlite, tests and git skills',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
